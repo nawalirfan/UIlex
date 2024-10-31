2a361f3e76df378f5160fd436870adaf639e3588
nawalirfan <nawal.irfan1110@gmail.com>
2024-10-31 16:57:30 +0500

    added reusable product card for ecommerce websites

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Main } from "./custom-components/Main";
 import { LoadingButtonPage } from "./reusable-components/buttons/loading-button/LoadingButtonPage";
 import { GradientButtonPage } from "./reusable-components/buttons/gradient-button/GradientButtonPage";
+import { ProductCardPage } from "./reusable-components/cards/product-card/ProductCardPage";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Main content={<LoadingButtonPage />} />} />
           <Route path="/buttons/loading-button" element={<Main content={<LoadingButtonPage />} />} />
           <Route path="/buttons/gradient-button" element={<Main content={<GradientButtonPage />} />} />
+          <Route path="/cards/product-card" element={<Main content={<ProductCardPage />} />} />
         </Routes>
       </Router>
     </SidebarProvider>
---
+++ src/reusable-components/cards/product-card/ProductCardPage.jsx
@@ -0,0 +1,36 @@
+import { productCardCode, productCardUsage } from "./code";
+import { useState } from "react";
+import CodeAndComponentViewer from "@/custom-components/CodeAndComponentViewer";
+import { Title } from "@/custom-components/Title";
+import { Card } from "@/components/ui/card";
+import ProductCard from "./ProductCard";
+
+export function ProductCardPage() {
+    const [loading, setLoading] = useState(false);
+
+    const handleAddToCart = () => {
+        setLoading(true);
+        setTimeout(() => setLoading(false), 1000);
+    };
+
+    return (
+        <>
+            <Title componentTitle="Loading Button" githubUsername="nawalirfan" githubLink="https://github.com/nawalirfan" />
+            <Card className="flex-grow h-full w-full mb-5">
+                <CodeAndComponentViewer
+                    component={<ProductCard
+                        title={'IPhone 16 Pro Max 256gb Desert Titanium'}
+                        imgLink={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMSGqMgjwSO-qaa6zk_fGPzrST0c7hWt4TdA&s'}
+                        price={6000}
+                        currency={'$'}
+                        rating={3}
+                        handleAddToCart={handleAddToCart}
+                        isLoading={loading}
+                    />}
+                    codeString={productCardCode}
+                    usageCodeString={productCardUsage}
+                />
+            </Card>
+        </>
+    );
+}
